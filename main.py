4390c6e0dea34926fed9e5c94031d8a4974b9f16
WDS <william_schor@brown.edu>
2020-02-28 11:39:06 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!~/.pyvenvs/todo_app
+# -*- coding: utf-8 -*-
+
+"""Module Summary
+
+Additional details...
+
+Functions
+-----------
+f(str, int): finds the int in the str and returns the index
+"""
+
+import os
+import sys
+
+
+def main(args):
+    print(os.name)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
